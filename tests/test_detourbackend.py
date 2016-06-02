702f2ae2bec84e3901a4c5f864ebc32cc475580a
antoniob <antoniob@cs.ucsb.edu>
2016-06-01 21:41:00 -0700

    different nose strategy

--- tests/test_detourbackend.py
+++ tests/test_detourbackend.py
@@ -17,7 +17,7 @@ qemu_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".
 
 global_data_fallback = None
 
-def add_fallback_test(f):
+def add_fallback_strategy(f):
     def wrapper():
         global global_data_fallback
         global_data_fallback = None
@@ -26,7 +26,7 @@ def add_fallback_test(f):
         f()
     return wrapper
 
-@add_fallback_test
+@add_fallback_strategy
 def test_simple_inline():
     filepath = os.path.join(bin_location, "cgc_scored_event_2/cgc/0b32aa01_01")
 
@@ -50,7 +50,7 @@ def test_simple_inline():
         nose.tools.assert_equal((res[0] == expected and p.returncode == 0), True)
 
 
-@add_fallback_test
+@add_fallback_strategy
 def test_added_code():
     filepath = os.path.join(bin_location, "cgc_scored_event_2/cgc/0b32aa01_01")
     pipe = subprocess.PIPE
@@ -74,7 +74,7 @@ def test_added_code():
         nose.tools.assert_equal(p.returncode == 0x32, True)
 
 
-@add_fallback_test
+@add_fallback_strategy
 def test_added_code_and_data():
     filepath = os.path.join(bin_location, "cgc_scored_event_2/cgc/0b32aa01_01")
     pipe = subprocess.PIPE
@@ -107,7 +107,7 @@ def test_added_code_and_data():
         nose.tools.assert_equal(test_str in res[0] and p.returncode == 0x33, True)
 
 
-@add_fallback_test
+@add_fallback_strategy
 def test_added_code_and_data_complex():
     filepath = os.path.join(bin_location, "cgc_trials/last_trial/original/CROMU_00070")
     pipe = subprocess.PIPE
@@ -236,7 +236,7 @@ def test_added_code_and_data_complex():
             nose.tools.assert_equal(expected == res[0] and p.returncode == -11, True)
 
 
-@add_fallback_test
+@add_fallback_strategy
 def test_added_code_and_data_big():
     filepath = os.path.join(bin_location, "cgc_scored_event_2/cgc/0b32aa01_01")
     pipe = subprocess.PIPE
@@ -269,7 +269,7 @@ def test_added_code_and_data_big():
         nose.tools.assert_equal(test_str in res[0] and p.returncode == 0x33, True)
 
 
-@add_fallback_test
+@add_fallback_strategy
 def test_detour():
     filepath = os.path.join(bin_location, "cgc_scored_event_2/cgc/0b32aa01_01")
     pipe = subprocess.PIPE
@@ -299,7 +299,7 @@ def test_detour():
         nose.tools.assert_equal(res[0], expected)
 
 
-@add_fallback_test
+@add_fallback_strategy
 def test_single_entry_point_patch():
     filepath = os.path.join(bin_location, "cgc_scored_event_2/cgc/0b32aa01_01")
     pipe = subprocess.PIPE
@@ -325,7 +325,7 @@ def test_single_entry_point_patch():
         nose.tools.assert_equal("\n\nEASTER EGG!\n\n" in res[0] and p.returncode == 0, True)
 
 
-@add_fallback_test
+@add_fallback_strategy
 def test_complex1():
     filepath = os.path.join(bin_location, "cgc_scored_event_2/cgc/0b32aa01_01")
     pipe = subprocess.PIPE
@@ -372,7 +372,7 @@ def test_complex1():
         nose.tools.assert_equal("\n\nEASTER EGG!\n\n"+test_str in res[0] and p.returncode == 52, True)
 
 
-@add_fallback_test
+@add_fallback_strategy
 def test_random_canary():
     def check_output(tstr):
         expected = "\nWelcome to Palindrome Finder\n\n\tPlease enter a possible palindrome: \t\tYes, that's a palindrome!\n\n\tPlease enter a possible palindrome: canary failure: 00000000 vs "
