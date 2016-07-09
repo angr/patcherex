6e8e2a8a57f03075acde852a0b107e0508b5aeae
Fish <fish@cs.ucsb.edu>
2016-07-09 08:02:52 -0700

    more test cases for reassembler

--- tests/test_reassemblerbackend.py
+++ tests/test_reassemblerbackend.py
@@ -1,6 +1,7 @@
 
 import os.path
-import multiprocessing
+import random
+from multiprocessing import Pool
 
 import nose.tools
 
@@ -54,43 +55,50 @@ def manual_run_functionality_all(threads=8):
             p = os.path.normpath(os.path.join('cgc_samples_multiflags', dirname, b))
             binaries.append(p)
 
+    random.shuffle(binaries)
+
     if threads > 1:
-
-        process_objects = [ ]
-        for thread_id in xrange(threads):
-            p = multiprocessing.Process(target=manual_run_functionality_core, args=(binaries, thread_id, threads))
-            process_objects.append(p)
-
-            p.start()
-
-        for p in process_objects:
-            p.join()
+        pool = Pool(threads, maxtasksperchild=10)
+        pool.map(manual_run_functionality_core, binaries, chunksize=1)
+        pool.close()
+        pool.join()
 
     else:
-        manual_run_functionality_core(binaries, 0, 1)
+        manual_run_functionality_core(binaries)
 
-def manual_run_functionality_core(binaries, thread_id, all_threads):
-    for i, b in enumerate(binaries):
-        if (i + thread_id) % all_threads != 0:
-            continue
+def set_proc_name(procname):
 
-        s = "[%02d] Reassembling %s..." % (thread_id, b)
-        print s
+    # for the ease of debugging...
+    # you can see the new name via "ps -A"
 
-        save_as = os.path.join("/",
-                               "tmp",
-                               "reassembled_binaries",
-                               os.path.basename(b),
-                               os.path.basename(os.path.dirname(b)),
-                               os.path.basename(b)
-                               )
-        try:
-            run_functionality(b, save_as=save_as)
-            print s, "succeeded"
-        except AssertionError:
-            print s, "failed"
-        except Exception as ex:
-            print s, "failed miserably with an exception: %s" % str(ex)
+    from ctypes import cdll, byref, create_string_buffer
+    libc = cdll.LoadLibrary('libc.so.6')
+    buff = create_string_buffer(len(procname) + 1)
+    buff.value = procname
+    libc.prctl(15, byref(buff), 0, 0, 0)
+
+def manual_run_functionality_core(b):
+    s = "Reassembling %s..." % (b)
+    print s
+
+    filename = os.path.basename(s)
+    set_proc_name(filename)
+
+
+    save_as = os.path.join("/",
+                            "tmp",
+                            "reassembled_binaries",
+                            os.path.basename(b),
+                            os.path.basename(os.path.dirname(b)),
+                            os.path.basename(b)
+                            )
+    try:
+        run_functionality(b, save_as=save_as)
+        print s, "succeeded"
+    except AssertionError:
+        print s, "failed"
+    except Exception as ex:
+        print s, "failed miserably with an exception: %s" % str(ex)
 
 #
 # Patching tests
@@ -151,16 +159,30 @@ def test_simple_pointer_encryption():
         os.path.join('cgc_trials', 'CROMU_00070'),
         os.path.join('cgc_trials', 'CROMU_00071'),
         os.path.join('cgc_trials', 'EAGLE_00005'),
+        #  os.path.join('cgc_samples_multiflags', 'CROMU_00001', 'original', 'CROMU_00001'),
     ]
 
     for b in binaries:
         run_simple_pointer_encryption(b)
 
-if __name__ == "__main__":
-    import logging
-    logging.getLogger('reassembler').setLevel(logging.DEBUG)
+#
+# Tracing
+#
 
+def trace():
+    import tracer
+
+    b = "/tmp/KPRCA_00025"
+    pov = "/home/fish/cgc/benign_traffic/KPRCA_00025/for-testing__GEN_00001.xml"
+
+    tracer = tracer.Tracer(b, pov_file=pov)
+
+if __name__ == "__main__":
+    # import logging
+    # logging.getLogger('reassembler').setLevel(logging.DEBUG)
+
+    # trace()
+    # manual_run_functionality_all(threads=8)
     test_simple_pointer_encryption()
-    # manual_run_functionality_all()
-    #test_functionality()
-    #test_shadowstack()
+    test_functionality()
+    test_shadowstack()
