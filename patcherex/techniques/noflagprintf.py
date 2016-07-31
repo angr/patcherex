25d3f949e3a17793bd8c5f44a4617702d67cad3a
Antonio Bianchi <antoniob@secloud-node-8.seclab.cs.ucsb.edu>
2016-07-31 04:45:35 -0700

    solve duplicate label problem - handle func_obj.format_spec_char equals None

--- patcherex/techniques/noflagprintf.py
+++ patcherex/techniques/noflagprintf.py
@@ -1,5 +1,8 @@
 import claripy
+import logging
 from patcherex.patches import AddLabelPatch, InsertCodePatch
+
+l = logging.getLogger("patcherex.techniques.NoFlagPrintfPatcher")
 
 # func name to format string arg position
 PRINTF_VARIANTS = {
@@ -34,8 +37,12 @@ class NoFlagPrintfPatcher(object):
         cfg = self.patcher.cfg
         patches = []
 
+        pnum = 0
         for func, (func_name, func_obj) in self.ident.matches.items():
             if func_name not in PRINTF_VARIANTS:
+                continue
+            if func_obj.format_spec_char is None:
+                l.warning("func_obj.format_spec_char is None")
                 continue
 
             fmt_arg_pos = PRINTF_VARIANTS[func_name]
@@ -63,46 +70,47 @@ class NoFlagPrintfPatcher(object):
                 # patch not necessary for this function
                 continue
 
+            pnum += 1
             check = """
                 ; is the address not in RO memory?
                 cmp dword [esp+{stack_offset}], {{max_ro_addr}}
-                jbe _end
+                jbe _end_printfcheck_%d
 
                 ; is the address in the flag page?
                 cmp dword [esp+{stack_offset}], {flag_page}
-                jb _check_for_percent
+                jb _check_for_percent_%d
                 cmp dword [esp+{stack_offset}], {flag_page_almost_end}
-                ja _check_for_percent
+                ja _check_for_percent_%d
 
                 ; they're trying to read from the flag page! fuck them.
                 jmp 0x41414141
 
-            _check_for_percent:
+            _check_for_percent_%d:
                 push esi ; = pointer to string
                 mov esi, [esp+{stack_offset_2}]
 
-            _loop_printfcheck:
+            _loop_printfcheck_%d:
                 cmp byte [esi], 0
-                je _restore_printfcheck
+                je _restore_printfcheck_%d
                 cmp byte [esi], {format_spec_char}
                 ; die!!!
                 je 0x41414141
                 inc esi
-                jmp _loop_printfcheck
+                jmp _loop_printfcheck_%d
 
-            _restore_printfcheck:
+            _restore_printfcheck_%d:
                 pop esi
 
-            _end:
+            _end_printfcheck_%d:
             """.format(
                 stack_offset=(fmt_arg_pos + 1) * 4,
                 stack_offset_2=(fmt_arg_pos + 2) * 4,
                 flag_page=FLAG_PAGE,
                 flag_page_almost_end=FLAG_PAGE + 0xffc,
                 format_spec_char=ord(func_obj.format_spec_char),
-            )
-
-            patches.append(InsertCodePatch(func.addr, check, priority=250))
+            ) % [pnum]*9
+            patches.append(InsertCodePatch(func.addr, check, priority=250, name="noflagprintf_%d" % pnum))
+            l.info("function at %#8x protected" % func.addr)
 
         if patches:
             max_ro_addr = max(seg.max_addr for seg in self.ro_segments)
