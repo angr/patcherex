6eeab909bf16d05f3264bd3fb28737f2ddadb41c
Fish <fish@cs.ucsb.edu>
2016-08-20 03:25:15 -0700

    revive SimplePointerEncryption

--- patcherex/techniques/__init__.py
+++ patcherex/techniques/__init__.py
@@ -4,3 +4,4 @@ from .cpuid import CpuId
 from .shiftstack import ShiftStack
 from .adversarial import Adversarial
 from .binary_optimization import BinaryOptimization
+from .simple_ptr_enc import SimplePointerEncryption
