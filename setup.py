e5c9f0aca06f095c28c59d1303c23e085163bc56
Yan <yans@yancomm.net>
2017-01-13 19:10:08 -0800

    small change to trigger ci

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='patcherex',
-    version='1.0',
+    version='1.1',
     description='The patcherex',
     packages=['patcherex'],
     install_requires=[
