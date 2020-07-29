50549a50c2e414bf73721f165d281e563b5fe5f9
Han Dai <github@daihan.me>
2020-07-29 05:25:21 +0000

    add PowerPC support for detourbackend

--- patcherex/backends/detourbackend.py
+++ patcherex/backends/detourbackend.py
@@ -16,8 +16,14 @@ def DetourBackend(filename, data_fallback=None, base_address=None, try_pdf_remov
                 detourbackendclass = getattr(importlib.import_module("patcherex.backends.detourbackends.aarch64"), "DetourBackendAarch64")
             elif start_bytes.startswith(b"\x28\x00", 0x12): # EM_ARM
                 detourbackendclass = getattr(importlib.import_module("patcherex.backends.detourbackends.arm"), "DetourBackendArm")
-            elif start_bytes.startswith(b"\x08\x00", 0x12) or start_bytes.startswith(b"\x00\x08", 0x12): # EM_MIPS
+            elif start_bytes.startswith(b"\x08\x00", 0x12) or \
+                 start_bytes.startswith(b"\x00\x08", 0x12): # EM_MIPS
                 detourbackendclass = getattr(importlib.import_module("patcherex.backends.detourbackends.mips"), "DetourBackendMips")
+            elif start_bytes.startswith(b"\x14\x00", 0x12) or \
+                 start_bytes.startswith(b"\x00\x14", 0x12) or \
+                 start_bytes.startswith(b"\x15\x00", 0x12) or \
+                 start_bytes.startswith(b"\x00\x15", 0x12): # EM_PPC / EM_PPC64
+                detourbackendclass = getattr(importlib.import_module("patcherex.backends.detourbackends.ppc"), "DetourBackendPpc")
             else:
                 raise Exception("Unsupported architecture.")
             return detourbackendclass(filename, base_address=base_address, replace_note_segment=replace_note_segment)
