83eb04ea5613e91074489c3bb5533be63c12c5ad
Han Dai <github@daihan.me>
2021-04-12 03:14:04 +0000

    remove backdoor_stuff

--- backdoor_stuff/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm backdoor_pov.pov
-rm backdoor_pov.o
-/usr/i386-linux-cgc/bin/clang -c -nostdlib -fno-builtin -nostdinc -Iinclude -Ilib -I/usr/include -O0 -g -Werror -Wno-overlength-strings -Wno-packed -Wall -o backdoor_pov.o backdoor_pov.c
-/usr/i386-linux-cgc/bin/ld -nostdlib -static -o backdoor_pov.pov backdoor_pov.o -L/usr/lib -lcgc -lpov
-
