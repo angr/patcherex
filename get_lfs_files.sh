7ec59d6b21b7fbeda5b3877596cb8a23f175c7d9
Antonio Bianchi <antoniob@purdue.edu>
2022-03-28 04:34:43 +0000

    add script for lfs files

---
+++ get_lfs_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://www.cs.purdue.edu/homes/antoniob/shared/precompiled_llvm_binaries.tgz
+tar -xf precompiled_llvm_binaries.tgz -C patcherex/prebuilt/
+rm precompiled_llvm_binaries.tgz
+
+
