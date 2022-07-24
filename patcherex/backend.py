eef9043847803f0ee78dc36cedaf5dfdbf926758
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2022-07-24 10:13:39 -0400

    damn you, linter

--- patcherex/backend.py
+++ patcherex/backend.py
@@ -16,19 +16,23 @@ FIND_FUNCS = (
 )
 
 def better_symbol_repr(sym: cle.Symbol):
+    """
+    Returns a string representation of a symbol containing all relevant information.
+    """
+    # pylint: disable=multiple-statements
     attrs = []
-    if sym.is_static: attrs.append('static')
-    if sym.is_common: attrs.append('common')
-    if sym.is_import: attrs.append('import')
-    if sym.is_export: attrs.append('export')
-    if sym.is_local: attrs.append('local')
-    if sym.is_weak: attrs.append('weak')
-    if sym.is_extern: attrs.append('extern')
-    if sym.is_forward: attrs.append('forward')
+    if sym.is_static:   attrs.append('static')
+    if sym.is_common:   attrs.append('common')
+    if sym.is_import:   attrs.append('import')
+    if sym.is_export:   attrs.append('export')
+    if sym.is_local:    attrs.append('local')
+    if sym.is_weak:     attrs.append('weak')
+    if sym.is_extern:   attrs.append('extern')
+    if sym.is_forward:  attrs.append('forward')
 
     return f"{sym.name=:<40} {sym.type=:<20} {sym.relative_addr=:<16} {sym.size=:<10} {attrs=}"
 
-class Backend(object):
+class Backend:
     """
     Patcher backend.
     """
@@ -46,7 +50,10 @@ class Backend(object):
         self.filename = filename
         self.try_pdf_removal = try_pdf_removal
         self.pdf_removed = False # has the pdf actually been removed?
-        self.project = angr.Project(filename, load_options={"auto_load_libs": False}, **project_options)
+        self.project = angr.Project(
+            filename,
+            load_options={"auto_load_libs": False},
+            **project_options)
         self._identifer = None
         with open(filename, "rb") as f:
             self.ocontent = f.read()
@@ -69,7 +76,8 @@ class Backend(object):
         """
         Save the patched binary onto disk.
 
-        :param str filename: The new file path to save to. If None, the original binary will be overwritten.
+        :param str filename: The new file path to save to. If None,
+                             the original binary will be overwritten.
         :return: None
         """
 
@@ -88,7 +96,8 @@ class Backend(object):
     @property
     def identifier(self):
         if self._identifer is None:
-            self._identifer = self.project.analyses.Identifier(self.cfg, require_predecessors=False)
+            cfg = self._generate_cfg()
+            self._identifer = self.project.analyses.Identifier(cfg, require_predecessors=False)
             list(self._identifer.run(only_find=FIND_FUNCS))
         return self._identifer
 
@@ -107,7 +116,8 @@ class Backend(object):
 
         # TODO
         # 1) ida-like cfg
-        # 2) with some strategies we don't need the cfg, we should be able to apply those strategies even if the cfg fails
+        # 2) with some strategies we don't need the cfg, we should be able
+        #    to apply those strategies even if the cfg fails
         l.info("CFG start...")
         cfg = self.project.analyses.CFGFast(normalize=True, data_references=True)
         l.info("... CFG end")
@@ -131,8 +141,11 @@ class Backend(object):
             return {}
 
         for sym in obj.symbols:
-            if sym.is_import or sym.type not in {cle.SymbolType.TYPE_FUNCTION, cle.SymbolType.TYPE_OBJECT}:
+            if sym.is_import:
                 continue
+            if sym.type not in {cle.SymbolType.TYPE_FUNCTION, cle.SymbolType.TYPE_OBJECT}:
+                continue
+
             if sym.relative_addr == 0:
                 r = better_symbol_repr(sym)
                 assert False, f"Symbol {sym!r} has relative address 0: {r}"
