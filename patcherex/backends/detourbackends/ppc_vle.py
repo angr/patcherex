7d8cf840361c3448ca2ad579c417904f3692074a
Han Dai <github@daihan.me>
2023-02-18 15:11:43 -0500

    init vle support

--- patcherex/backends/detourbackends/ppc_vle.py
+++ patcherex/backends/detourbackends/ppc_vle.py
@@ -37,11 +37,135 @@ class DetourBackendPpcVle(DetourBackendPpc):
         if try_reuse_unused_space:
             raise NotImplementedError()
         super().__init__(filename, base_address=base_address, replace_note_segment=replace_note_segment, try_without_cfg=try_without_cfg, use_pickle=use_pickle, skip_super_init=True)
-        self.added_code_segment = 0xf00000
-        self.added_data_segment = 0xe00000
+        self.added_code_segment = 0x840000
+        self.added_data_segment = 0x850000
         self.name_map.update(ADDED_DATA_START = (len(self.ncontent) % 0x1000) + self.added_data_segment)
 
-    def compile_asm(self, code, base=None, name_map=None, is_thumb=False):
+
+    def dump_sections(self):
+        with open(self.filename, "rb") as f:
+            elf = ELFFile(f)
+            sections = []
+            for i in range(elf.num_sections()):
+                sec = elf.get_section(i)
+                sections.append(sec)
+        return sections
+
+    def apply_patches(self, patches):
+        super().apply_patches(patches)
+        # reuse .debug_info and .debug_abbrev sections for new code and data
+        current_Ehdr = self.structs.Elf_Ehdr.parse(self.ncontent)
+        self.sections = self.dump_sections()
+        for section in enumerate(self.sections):
+            if section[1].name == ".debug_info":
+                current_Shdr = section[1].header
+                current_Shdr['sh_addr'] = self.name_map["ADDED_CODE_START"]
+                current_Shdr['sh_offset'] = self.added_code_file_start
+                current_Shdr['sh_size'] = len(self.added_code)
+                current_Shdr['sh_flags'] |= 0x2
+                self.ncontent = utils.bytes_overwrite(self.ncontent, self.structs.Elf_Shdr.build(current_Shdr), current_Ehdr['e_shoff'] + current_Ehdr['e_shentsize'] * section[0])
+            if section[1].name == ".debug_abbrev":
+                current_Shdr = section[1].header
+                current_Shdr['sh_addr'] = self.name_map["ADDED_DATA_START"]
+                current_Shdr['sh_flags'] |= 0x2
+                current_Shdr['sh_offset'] = self.added_data_file_start
+                current_Shdr['sh_size'] = len(self.added_data)
+                self.ncontent = utils.bytes_overwrite(self.ncontent, self.structs.Elf_Shdr.build(current_Shdr), current_Ehdr['e_shoff'] + current_Ehdr['e_shentsize'] * section[0])
+
+
+    def setup_headers(self, segments):
+        pass
+
+    def set_added_segment_headers(self):
+        pass
+
+    def compile_moved_injected_code_for_insertfunctionpatch(self, classified_instructions, patch, offset=0, is_thumb=False):
+        # Prepare pre_code with dummy function address
+        pre_code = "\n".join([self.capstone_to_asm(i)
+                                    for i in classified_instructions
+                                    if i.overwritten == 'pre'])
+        pre_code += "\n"
+        # store all general purpose registers on the stack
+        pre_code += "e_stwu r1, -0x80(r1)\n"
+        pre_code += "e_stmw r3, 0x8(r1)\n"
+
+        pre_code += patch.prefunc + "\n"
+        pre_code = "\n".join([line for line in pre_code.split("\n") if line != ""])
+
+        # compile pre_code
+        pre_code_compiled = self.compile_asm(pre_code,
+                                              base=self.get_current_code_position(),
+                                              name_map=self.name_map)
+
+        # compile function_call to get the size of it
+        fake_function_call = f"e_bl {hex(self.get_current_code_position() + 0x40)}\n"
+        fake_function_call_compiled = self.compile_asm(fake_function_call,
+                                              base=self.get_current_code_position() + len(pre_code_compiled),
+                                              name_map=self.name_map)
+
+        # prepare post code
+        post_code = "e_lmw r3, 0x8(r1)\n"
+        post_code += "e_addi r1, r1, 0x80\n"
+        post_code += "\n".join([self.capstone_to_asm(i)
+                                    for i in classified_instructions
+                                    if i.overwritten == 'culprit'])
+        post_code += "\n"
+        post_code += "\n".join([self.capstone_to_asm(i)
+                                    for i in classified_instructions
+                                    if i.overwritten == 'post'])
+        post_code += "\n"
+        if "RESTORE_CONTEXT" in patch.postfunc:
+            post_code = patch.postfunc.replace("RESTORE_CONTEXT", post_code)
+        else:
+            post_code = patch.postfunc + "\n" + post_code
+        jmp_back_target = None
+        for i in reversed(classified_instructions):  # jmp back to the one after the last byte of the last non-out
+            if i.overwritten != "out":
+                jmp_back_target = i.address+len(i.bytes)
+                break
+        assert jmp_back_target is not None
+        post_code += "e_b %s" % hex(int(jmp_back_target) - offset) + "\n"
+        post_code = "\n".join([line for line in post_code.split("\n") if line != ""])
+
+        # compile post_code
+        post_code_compiled = self.compile_asm(post_code,
+                                              base=self.get_current_code_position() + len(pre_code_compiled) + len(fake_function_call_compiled),
+                                              name_map=self.name_map)
+        jmp_table_addr = self.get_current_code_position() + len(pre_code_compiled) + len(fake_function_call_compiled)
+        jmp_table_instrs = self.disassemble(post_code_compiled, jmp_table_addr)
+        exit_edges = []
+        for idx in range(len(jmp_table_instrs)):
+            instr = jmp_table_instrs[idx]
+            if instr.mnemonic.startswith("e_lmw"):
+                break
+            if instr.mnemonic == "se_cmpi":
+                if idx < len(jmp_table_instrs) - 1 and jmp_table_instrs[idx + 1].mnemonic.startswith("b"):
+                    next_instr = jmp_table_instrs[idx + 1]
+                    ret_val = instr.operands[1].imm
+                    if ret_val != 0:
+                        exit_edges.append([hex(jmp_table_addr), hex(next_instr.operands[0].imm)])
+        self.patch_info["exit_edges"]["patched"][hex(self.project.kb.functions.floor_func(patch.addr).addr)] = exit_edges
+
+        if (self.get_current_code_position() + len(pre_code_compiled) + len(fake_function_call_compiled) + len(post_code_compiled)) % 4 != 0:
+            post_code_compiled += b"\x00"*(4 - (self.get_current_code_position() + len(pre_code_compiled) + len(fake_function_call_compiled) + len(post_code_compiled)) % 4)
+
+        # recompile function call with the correct function address
+        function_call = f"e_bl {hex(self.get_current_code_position() + len(pre_code_compiled) + len(fake_function_call_compiled) + len(post_code_compiled))}\n"
+        function_call_compiled = self.compile_asm(function_call,
+                                              base=self.get_current_code_position() + len(pre_code_compiled),
+                                              name_map=self.name_map)
+
+        # compile the function itslef
+        patch_info_addr = self.get_current_code_position() + len(pre_code_compiled) + len(function_call_compiled) + len(post_code_compiled)
+        self.patch_info["cfgfast_options"]["patched"]["function_starts"].append(hex(patch_info_addr))
+        self.patch_info["patcherex_added_functions"].append(hex(patch_info_addr))
+        func_code_compiled = self.compile_function(patch.func, compiler_flags="-fPIE" if self.project.loader.main_object.pic else "", entry=self.get_current_code_position() + len(pre_code_compiled) + len(function_call_compiled) + len(post_code_compiled), symbols=patch.symbols)
+        if len(func_code_compiled) % 4 != 0:
+            func_code_compiled += b"\x00"*(4 - len(func_code_compiled) % 4)
+
+        return pre_code_compiled + function_call_compiled + post_code_compiled + func_code_compiled
+
+    def compile_asm(self, code, base=None, name_map=None, is_thumb=False, dummy=False):
         try:
             if name_map is not None:
                 code = code.format(**name_map)  # compile_asm
@@ -50,11 +174,45 @@ class DetourBackendPpcVle(DetourBackendPpc):
         except KeyError as e:
             raise UndefinedSymbolException(str(e)) from e
 
+        code = re.subn(r'r(\d+)', r'\1', code)[0]
+
+        if base is not None and not dummy:
+            # produce a list of {instr_offset: instr} pairs
+            branch_instrs = {}
+            instr_count = 0
+            for line in code.splitlines():
+                line = line.strip()
+                if line.startswith(".") or line.startswith("#") or line == "" or line.endswith(":"):
+                    continue
+                # if line matches "e_b 0x*" or "e_bl 0x*", add it to the branch_instrs dict
+                if re.match(r"(e_b|e_bl) 0x[0-9a-fA-F]+", line):
+                    branch_instrs[instr_count] = line
+                instr_count += 1
+
+            disasms = self.disassemble(self.compile_asm(code, base=base, name_map=name_map, dummy=True), offset=base)
+
+            for i in range(len(disasms)):
+                if i in branch_instrs:
+                    branch_instrs[i] = branch_instrs[i].split(" ")[0] + " " + hex(int(branch_instrs[i].split(" ")[1], 16) - disasms[i]['address'])
+
+            instr_count = 0
+            for line_count, line in enumerate(code.splitlines()):
+                if line.startswith(".") or line.startswith("#") or line == "" or line.endswith(":"):
+                    continue
+                if instr_count in branch_instrs:
+                    code = code.splitlines()
+                    code[line_count] = branch_instrs[instr_count]
+                    code = "\n".join(code)
+                instr_count += 1
+
+        # set base address
+        if base is not None:
+            code = f".org {hex(base)}\n" + code
+
         # use `as` to assemble the code
         with utils.tempdir() as td:
             with open(os.path.join(td, "code.s"), "w") as f:
                 f.write(code)
-                print(code)
             res = utils.exec_cmd("/home/han/misc/powerpc-eabivle-4_9/bin/powerpc-eabivle-as -mvle -o %s.o %s" % (os.path.join(td, "code"), os.path.join(td, "code.s")), shell=True)
             if res[2] != 0:
                 raise Exception(f"powerpc-eabivle-as:\n{str(res[0] + res[1], 'utf-8')}")
@@ -62,7 +220,21 @@ class DetourBackendPpcVle(DetourBackendPpc):
             if res[2] != 0:
                 raise Exception(f"powerpc-eabivle-objcopy:\n{str(res[0] + res[1], 'utf-8')}")
             with open(os.path.join(td, "code.bin"), "rb") as f:
-                return f.read()
+                if base is not None:
+                    f.seek(base)
+                result = f.read()
+                disasms = self.disassemble(result, base if base is not None else 0x0)
+                return result
+
+    def check_if_movable(self, instruction, is_thumb=False):
+        def bytes_to_comparable_str(ibytes, offset):
+            return " ".join(utils.instruction_to_str(self.disassemble(ibytes, offset)[0]).split()[2:])
+
+        instruction_bytes = instruction.bytes
+        pos1 = bytes_to_comparable_str(instruction_bytes, 0x0)
+        pos2 = bytes_to_comparable_str(instruction_bytes, 0x840000)
+        pos3 = bytes_to_comparable_str(instruction_bytes, 0x1040000)
+        return pos1 == pos2 and pos2 == pos3
 
     def disassemble(self, code, offset=0x0, is_thumb=False):
         if isinstance(code, str):
@@ -71,7 +243,7 @@ class DetourBackendPpcVle(DetourBackendPpc):
             with open(os.path.join(td, "code.bin"), "wb") as f:
                 f.write(code)
 
-            res = utils.exec_cmd("/home/han/misc/powerpc-eabivle-4_9/bin/powerpc-eabivle-objdump -D -b binary -m powerpc:vle -EB %s.bin | tail +8" % (os.path.join(td, "code")), shell=True)
+            res = utils.exec_cmd("/home/han/misc/powerpc-eabivle-4_9/bin/powerpc-eabivle-objdump -D -b binary --adjust-vma=%s -m powerpc:vle -EB %s.bin | tail +8" % (hex(offset), os.path.join(td, "code")), shell=True)
             if res[2] != 0:
                 raise Exception(f"powerpc-eabivle-objdump:\n{str(res[0] + res[1], 'utf-8')}")
             str_result = res[0].decode("utf-8")
@@ -81,7 +253,7 @@ class DetourBackendPpcVle(DetourBackendPpc):
             m = re.match(r"\s+(?P<address>[0-9a-f]+):\s+(?P<bytes>([0-9a-f]{2}\s)+)\s+(?P<mnemonic>.+?)\s+(?P<op_str>.+?)$", line)
             if m:
                 instr = AttrDict(m.groupdict())
-                instr['address'] = int(instr['address'], 16) + offset
+                instr['address'] = int(instr['address'], 16)
                 instr['bytes'] = bytes.fromhex(instr['bytes'])
                 instr['mnemonic'] = re.sub(r'\s+','', instr['mnemonic'])
                 instr['op_str'] = re.sub(r'\s+','', instr['op_str'].split(";")[0]).replace(",", ", ")
@@ -203,3 +375,52 @@ class DetourBackendPpcVle(DetourBackendPpc):
             else:
                 compiled = ld.memory.load(ld.all_objects[0].entry, text_section_size)
                 return compiled
+
+    @staticmethod
+    def generate_asm_jump_on_return_val(mapping):
+        asm = ""
+        for ret_val, jmp_addr in mapping.items():
+            asm += f"se_cmpi r3, {ret_val}\n"
+            asm += f"e_beq _label_{ret_val}\n"
+        asm += f"RESTORE_CONTEXT\n"
+        asm += f"e_b _end\n"
+        for ret_val, jmp_addr in mapping.items():
+            asm += f"_label_{ret_val}:\n"
+            asm += f"RESTORE_CONTEXT\n"
+            asm += f"e_b {hex(jmp_addr)}\n"
+        asm += f"_end:\n"
+        return asm
+    
+    @staticmethod
+    def generate_asm_for_arguments(arg_list):
+        if len(arg_list) == 0:
+            return ""
+        if len(arg_list) > 4:
+            raise Exception("Currently Patcherex Only Supports Up to 4 Arguments")
+        asm = ""
+        const_pool = ""
+        for arg in arg_list:
+            _const_pool = re.search(r"e_b _end\n(.*)_end:", arg, re.DOTALL)
+            if _const_pool:
+                const_pool += f"{_const_pool.group(1)}\n"
+
+
+        for i in range(1, len(arg_list)):
+            if "e_b _end" in arg_list[i]:
+                asm += f"{arg_list[i][:arg_list[i].index('e_b _end')]}\n"
+            else:
+                asm += f"{arg_list[i]}\n"
+            asm += f"se_mr r{i+3}, r3\n"
+
+        if "e_b _end" in arg_list[0]:
+            asm += f"{arg_list[0][:arg_list[0].index('e_b _end')]}\n"
+        else:
+            asm += f"{arg_list[0]}\n"
+
+        if const_pool:
+            const_pool_list = const_pool.splitlines()
+            const_pool = "\n".join(sorted(set(const_pool_list), key=const_pool_list.index))
+            asm += "b _end\n"
+            asm += const_pool
+            asm += "_end:\n"
+        return asm
