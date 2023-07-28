f4b445546204752ae77639e62412ad6d874a1f8b
Han Dai <github@daihan.me>
2023-07-28 06:02:33 -0400

    better address for ppc

--- patcherex/backends/detourbackends/ppc.py
+++ patcherex/backends/detourbackends/ppc.py
@@ -27,8 +27,8 @@ class DetourBackendPpc(DetourBackendElf):
         if try_reuse_unused_space:
             raise NotImplementedError()
         super().__init__(filename, base_address=base_address, replace_note_segment=replace_note_segment, try_without_cfg=try_without_cfg, use_pickle=use_pickle, skip_super_init=skip_super_init)
-        self.added_code_segment = 0x10600000
-        self.added_data_segment = 0x10700000
+        self.added_code_segment = 0x840000
+        self.added_data_segment = 0x850000
         self.name_map.update(ADDED_DATA_START = (len(self.ncontent) % 0x1000) + self.added_data_segment)
 
     def get_oep(self):
